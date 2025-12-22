e79415b6d09b387a6a88c9ea5e1bb5f46fe9cd3f
Chinagolum <chinagolum_adigwe@outlook.com>
2025-12-22 16:27:25 +0200

    Add dancing bear app to monorepo

---
+++ DancingBear/DancingBear/DancingBearApp.swift
@@ -0,0 +1,17 @@
+//
+//  DWB3App.swift
+//  DWB3
+//
+//  Created by abcd on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct DWB3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
