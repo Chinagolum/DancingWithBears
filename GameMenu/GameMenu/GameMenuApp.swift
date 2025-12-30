da10e778863c6e5c2abb60df0b573f9d1af38614
Chinagolum <chinagolum_adigwe@outlook.com>
2025-12-30 04:24:27 +0200

    Game menu barebones

---
+++ GameMenu/GameMenu/GameMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameMenuApp.swift
+//  GameMenu
+//
+//  Created by Chinagolum Adigwe on 12/28/25.
+//
+
+import SwiftUI
+
+@main
+struct GameMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
